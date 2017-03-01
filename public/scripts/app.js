fb96e9b3708857a28ea0b223c0028085787b0e52
Fiodor Gorobet <casusarts@gmail.com>
2017-03-01 22:39:54 +0100

    change api provider

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,23 @@
+// const weather = function (lan, lon) {
+//   this.lan = lan;
+//   this.lon = lon;
+//
+//   const lang = navigator.language;
+//   const settings = {
+//     "url": "https://community-open-weather-map.p.mashape.com/weather?callback=weatherdata?" + lang + "&lat=" + lat + "&lon=" + lon + "q=q?units=metric&APPID=7365a15a8446e775127e7f852435d16f",
+//     "method": "GET",
+//     "header": {
+//       "X-Mashape-Key": "P319rwetHomshMk3cmoB2FmDJkPFp174LS7jsnqrvgi4RdpfWE",
+//       "accept": "application/json"
+//     }
+//   }
+//
+//   $.ajax(settings).done(function (json) {
+//     console.log(json);
+//   });
+// }
+
 $(document).ready(function () {
-  //var geo = document.getElementById("geo");
 
   var options = {
     enableHighAccuracy: true,
@@ -9,23 +27,44 @@ $(document).ready(function () {
 
   function success(pos) {
     var html = '';
-    const API_KEY = 'a8f6d380da1dc07ada4a449108144813';
+    //const API_KEY = 'a8f6d380da1dc07ada4a449108144813';
 
     var crd = pos.coords;
     var lat = crd.latitude;
     var lon = crd.longitude;
 
-    var loc = [lat, lon];
+    const lang = navigator.language;
+    const settings = {
+      "url": "https://community-open-weather-map.p.mashape.com/weather?callback=weatherdata?" + lang + "&lat=" + lat + "&lon=" + lon + "&q=q&units=metric&APPID=7365a15a8446e775127e7f852435d16f",
+      "method": "GET",
+      "header": {
+        "X-Mashape-Key": "wfwWLSXQeqmshrbvVrM8uHmO1wtKp1x0LSbjsnVZNTyJr6IhCx",
+        "accept": "application/json"
+      }
+    }
 
-    // const weahter = $.get(
-    //   "https://api.darksky.net/forecast/" + encodeURI(API_KEY)  + '/' + encodeURI(lat) + ',' + encodeURI(lon)
-    // );
-
-    const weather = $.getJSON('../data/city.list.json', function (json) {
-      // return json;
+    $.ajax(settings).done(function (json) {
+      console.log(json);
     });
+    //var loc = [lat, lon];
 
-    console.log(weahter);
+    // const settings = {
+    //   "async": true,
+    //   "crossDomain": true,
+    //   "url": "https://api.darksky.net/forecast/" + encodeURI(API_KEY + '/' + lat + ',' + lon),
+    //   "method": "GET",
+    //   "header": {
+    //     "content-type": "application/x-www-form-urlencoded",
+    //     "accept": "application/json",
+    //     "Access-Control-Allow-Origin": "*"
+    //   }
+    // }
+    //
+    //
+    // $.ajax(settings).done(function (json) {
+    //   console.log(json);
+    // });
+
     html += "<strong>Lat:</strong> " + lat + " " + "<strong>Lon:</strong> " + lon;
     $('.location').html(html);
   }
